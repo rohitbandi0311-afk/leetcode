9f175e3e34d07f44b21de020f0e328eb88ac93b3
Rohith Bandi <rohit.bandi0311@gmail.com>
2026-08-31 17:08:35 +0530

    Time: 3 ms (67.35%) | Memory: 124.6 MB (60.57%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        if (!head || !head->next || !head->next->next) {
+            return {-1, -1};
+        }
+
+        int firstCritical = -1;
+        int prevCritical = -1;
+        int minDistance = INT_MAX;
+        
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        int index = 1;
+
+        while (curr->next != nullptr) {
+            // Check if current node is a local maxima or local minima
+            if ((curr->val > prev->val && curr->val > curr->next->val) ||
+                (curr->val < prev->val && curr->val < curr->next->val)) {
+                
+                if (firstCritical == -1) {
+                    firstCritical = index;
+                } else {
+                    minDistance = min(minDistance, index - prevCritical);
+                }
+                prevCritical = index;
+            }
+            
+            prev = curr;
+            curr = curr->next;
+            index++;
+        }
+
+        // If fewer than 2 critical points are found
+        if (firstCritical == -1 || prevCritical == firstCritical) {
+            return {-1, -1};
+        }
+
+        int maxDistance = prevCritical - firstCritical;
+        return {minDistance, maxDistance};
+    }
+};
